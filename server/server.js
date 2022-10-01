de3842aea51fc0240bf9ff229e716a226028f998
Markhan1 <markhan1@protonmail.com>
2022-10-01 13:06:11 +0800

    Updated server.js and package.json

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const dbo = require("./db/conn");
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.json({ status: 200 });
+});
+
+app.listen(port, () => {
+  dbo.connectToServer((err) => {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
