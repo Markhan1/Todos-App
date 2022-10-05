4b08dff90ad7a17ebd3aedb510af8a2876da36ad
Markhan1 <markhan1@protonmail.com>
2022-10-05 13:05:40 +0800

    constructing firebase login feature into client server

---
+++ client/src/Context/AuthContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(false);
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    logout
+  }
+
+  return(
+    <AuthContext.Provider>
+      { !loading && children }
+    </AuthContext.Provider>
+  );
+}
